8c05b01b62502d71e5acf3e515b2bb2f5ffb1c47
raphaella-rose <raphaella.rose1@gmail.com>
2024-05-15 12:25:05 +0100

    make code worse

--- credit-score.ts
+++ credit-score.ts
@@ -2,28 +2,28 @@ import { CreditReport, CreditScore } from "./types";
 
 /**
  *
- * @param creditReport the credit report for a user
+ * @param c the credit report for a user
  * @returns a credit score with a value and category
  */
-export const getCreditScore = (creditReport: CreditReport): CreditScore => {
-  const creditUtilisation = creditReport.creditUtilisationPercentage;
+export const getCreditScore = (c: CreditReport): CreditScore => {
+  const x = c.creditUtilisationPercentage;
 
-  if (creditUtilisation > 0.3 && creditUtilisation <= 0.5) {
+  if (x > 0.3 && x <= 0.5) {
     return {
       value: 960,
       category: "good",
     };
-  } else if (creditUtilisation > 0.5 && creditUtilisation <= 0.7) {
+  } else if (x > 0.5 && x <= 0.7) {
     return {
       value: 880,
       category: "fair",
     };
-  } else if (creditUtilisation > 0.7 && creditUtilisation <= 0.9) {
+  } else if (x > 0.7 && x <= 0.9) {
     return {
       value: 720,
       category: "poor",
     };
-  } else if (creditUtilisation > 0.9) {
+  } else if (x > 0.9) {
     return {
       value: 560,
       category: "very poor",
