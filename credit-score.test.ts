893bddd59e3c02bc1d59872f8df5a9b45c8bb865
Raphaella D'Alessandro <raphaella.alessandro@oaknorth.co.uk>
2024-05-15 10:43:28 +0100

    update test descriptions (#5)

--- credit-score.test.ts
+++ credit-score.test.ts
@@ -2,7 +2,7 @@ import { expect, describe, it } from "vitest";
 import { getCreditScore } from "./credit-score";
 
 describe("getCreditScore", () => {
-  it("should return 300 if the credit utilisation is more than 90%", () => {
+  it("should return 560 if the credit utilisation is more than 90%", () => {
     const creditReport = {
       paymentHistory: [],
       creditUtilisationPercentage: 0.95,
@@ -14,7 +14,7 @@ describe("getCreditScore", () => {
     expect(creditScore.category).toBe("very poor");
   });
 
-  it("should return 500 if the credit utilisation is between 70% and 90%", () => {
+  it("should return 720 if the credit utilisation is between 70% and 90%", () => {
     const creditReport = {
       paymentHistory: [],
       creditUtilisationPercentage: 0.8,
@@ -24,7 +24,7 @@ describe("getCreditScore", () => {
     expect(creditScore.category).toBe("poor");
   });
 
-  it("should return 600 if the credit utilisation is between 50% and 70%", () => {
+  it("should return 880 if the credit utilisation is between 50% and 70%", () => {
     const creditReport = {
       paymentHistory: [],
       creditUtilisationPercentage: 0.6,
@@ -34,7 +34,7 @@ describe("getCreditScore", () => {
     expect(creditScore.category).toBe("fair");
   });
 
-  it("should return 700 if the credit utilisation is between 30% and 50%", () => {
+  it("should return 960 if the credit utilisation is between 30% and 50%", () => {
     const creditReport = {
       paymentHistory: [],
       creditUtilisationPercentage: 0.4,
@@ -44,7 +44,7 @@ describe("getCreditScore", () => {
     expect(creditScore.category).toBe("good");
   });
 
-  it("should return 900 if the credit utilisation is less than 30%", () => {
+  it("should return 999 if the credit utilisation is less than 30%", () => {
     const creditReport = {
       paymentHistory: [],
       creditUtilisationPercentage: 0.2,
